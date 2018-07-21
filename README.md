bc3eb8daa6b46aaa6720540d1380be050f3f7169
Hernán Vargas <hernan.vargas@alumnos.usm.cl>
2018-07-20 22:59:58 -0400

    comentarios y readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# inf564-tarea3
+# INF564 Tarea 3
+
+## Compilación
+Para compilar ejecutar sbt, luego:
+    compile
+    package
+
+## Ejecución
+Se puede ejecutar el código directamente desde `sbt` con:
+    run [filename.csv] [k] [EPS] [MinPts]
+
+O directamente del paquete:
+    spark-submit target/scala-2.11/dsnn_2.11-1.0.jar [filename.csv] [k] [EPS] [MinPts]
+
+Si no es envian argumentos el programa funcionará con los parametros por defecto, es decir su ejecución será equivalente a :
+    run bitcoinalpha.csv 10 3 3
--- src/main/scala/dsnn.scala
+++ src/main/scala/dsnn.scala
@@ -5,6 +5,8 @@ object dsnn {
   import org.apache.spark._
   import org.apache.spark.graphx.{Graph, Edge}
 
+  /* Toma dos lista ordenadas de nodos y retorna una lista ordenada con los k valores
+   * menores. De esta forma se calculan los k vecinos mas cercanos. */
   def kMin (k: Int, a: Array[(Long, Int)], b: Array[(Long, Int)]): Array[(Long, Int)] = {
     var i = 0; var j = 0; var r = Array[(Long, Int)]()
     while (i < a.length || j < b.length) {
@@ -22,6 +24,8 @@ object dsnn {
     r
   }
 
+  /* Calcula la vecindad compartida entre a y b, retorna el numero de elementos
+   * que estan en ambos conjuntos. */
   def SNN (a: Array[(Long, Int)], b: Array[(Long, Int)]): Int = {
     var c = 0
     for (x <- a; y <- b) {
@@ -33,35 +37,44 @@ object dsnn {
   def main (args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("dsnn").setMaster("local[2]")
     val sc = new SparkContext(sparkConf)
-    val file = "bitcoinalpha.csv"
-    //val file = "ezbit.csv"
-    val k = 4
-    val eps = 1
-    val minpts = 2
-    println("--------------------------------------------")
-    args.foreach(println)
+    // Parametros del algoritmo:
+    var file   = "bitcoinalpha.csv"
+    var k      = 10
+    var eps    = 3
+    var minpts = 3
+    if (args.length > 0) file   = args(0)
+    if (args.length > 1) k      = args(1).toInt
+    if (args.length > 2) eps    = args(2).toInt
+    if (args.length > 3) minpts = args(3).toInt
+    println("| file: "+file+" | k = "+k+" | EPS = " + eps + " | MinPts = " + minpts +" |")
 
     //Se lee el archivo y se transforma en un conjunto de arcos
     val lines = sc.textFile(file).map(line => line.split(','))
     val data = lines.map( arr => {
+      // Se transforma el rating a distancia.
       Edge[Int](arr(0).toLong, arr(1).toLong, 10 - arr(2).toInt)
     })
+
     // Los arcos se transforman en un grafo.
     val graph = Graph.fromEdges[Int, Int](data, 0).cache()
-    //println(graph.numEdges)
-    //println(graph.numVertices)
+
     // Se calculan los k vecinos más cercanos a cada nodo.
     val kclosest = graph.aggregateMessages[Array[(Long, Int)]](
       triplet => { triplet.sendToSrc( Array((triplet.dstId, triplet.attr)) ) },
       (a,b) => { kMin(k, a, b) }
     )
 
-    //kclosest.foreach(x => println(x._1, x._2.deep))
-    var snns = kclosest.cartesian(kclosest).map( x => { (x._1._1, SNN(x._1._2,x._2._2)) })
+    // Se crea la matriz con la vecindad compartida
+    var snns = kclosest.cartesian(kclosest).filter(z=>{z._1._1 != z._2._1}).map(x => {
+      (x._1._1, SNN(x._1._2,x._2._2))
+    })
+
+    // Se obtienen los core-points
     var core = snns.filter(x=>{x._2>eps}).countByKey.filter(x=>{x._2>minpts}) //core points
+
+    println("core points:")
     println(core.keys)
 
-    println("--------------------------------------------")
     sc.stop()
   }
 }
